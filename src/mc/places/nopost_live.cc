dbc82a76e10dec34870e720b44e133c10030a7a4
Alexandre Hamez <alexandre.hamez@gmail.com>
2014-01-19 11:05:39 +0100

    Take into account transitions with no post when computing dead transitions for places encoding.

--- src/mc/places/nopost_live.cc
+++ src/mc/places/nopost_live.cc
@@ -1,9 +1,57 @@
-//
-//  nopost_live.cc
-//  caesar.sdd
-//
-//  Created by Alexandre Hamez on 19/01/2014.
-//  Copyright (c) 2014 ISAE. All rights reserved.
-//
+#include <ostream>
 
-#include "nopost_live.h"
+#include "mc/places/nopost_live.hh"
+
+namespace pnmc { namespace mc { namespace places {
+
+/*------------------------------------------------------------------------------------------------*/
+
+nopost_live::nopost_live(unsigned int transition, boost::dynamic_bitset<>& bitset)
+  : transition(transition), bitset(bitset)
+{}
+
+/*------------------------------------------------------------------------------------------------*/
+
+sdd::values::bitset<64>
+nopost_live::operator()(const sdd::values::bitset<64>& val)
+const noexcept
+{
+  bitset[transition] = true;
+  return val;
+}
+
+/*------------------------------------------------------------------------------------------------*/
+
+std::ostream&
+operator<<(std::ostream& os, const nopost_live& p)
+{
+  return os << "nopost()";
+}
+
+/*------------------------------------------------------------------------------------------------*/
+
+/// @brief Equality of two nopost_live.
+bool
+operator==(const nopost_live& lhs, const nopost_live& rhs) noexcept
+{
+  return lhs.transition == rhs.transition;
+}
+
+/*------------------------------------------------------------------------------------------------*/
+
+}}} // namespace pnmc::mc::places
+
+namespace std {
+
+/*------------------------------------------------------------------------------------------------*/
+
+std::size_t
+hash<pnmc::mc::places::nopost_live>::operator()(const pnmc::mc::places::nopost_live& p)
+const noexcept
+{
+  return sdd::util::hash(p.transition);
+}
+
+/*------------------------------------------------------------------------------------------------*/
+
+} // namespace std
--- src/mc/places/nopost_live.hh
+++ src/mc/places/nopost_live.hh
@@ -1,14 +1,57 @@
-//
-//  nopost_live.h
-//  caesar.sdd
-//
-//  Created by Alexandre Hamez on 19/01/2014.
-//  Copyright (c) 2014 ISAE. All rights reserved.
-//
+#ifndef _PNMC_MC_PLACES_NOPOST_LIVE_HH_
+#define _PNMC_MC_PLACES_NOPOST_LIVE_HH_
 
-#ifndef __caesar_sdd__nopost_live__
-#define __caesar_sdd__nopost_live__
+#include <functional> // hash
+#include <iosfwd>
 
-#include <iostream>
+#include <boost/dynamic_bitset.hpp>
 
-#endif /* defined(__caesar_sdd__nopost_live__) */
+#include "sdd/values/bitset.hh"
+
+namespace pnmc { namespace mc { namespace places {
+
+/*------------------------------------------------------------------------------------------------*/
+
+struct nopost_live
+{
+  /// @brief Transition index.
+  const unsigned int transition;
+
+  /// @brief The bitset to update.
+  boost::dynamic_bitset<>& bitset;
+
+  /// @brief Constructor.
+  nopost_live(unsigned int transition, boost::dynamic_bitset<>& bitset);
+
+  /// @brief Apply this operation.
+  sdd::values::bitset<64>
+  operator()(const sdd::values::bitset<64>&) const noexcept;
+};
+
+/// @brief Equality of two nopost_live.
+bool
+operator==(const nopost_live& lhs, const nopost_live& rhs) noexcept;
+
+/// @brief Textual output of a nopost_live.
+std::ostream&
+operator<<(std::ostream&, const nopost_live&);
+
+/*------------------------------------------------------------------------------------------------*/
+
+}}} // namespace pnmc::mc::places
+
+namespace std {
+
+/*------------------------------------------------------------------------------------------------*/
+
+template <>
+struct hash<pnmc::mc::places::nopost_live>
+{
+  std::size_t operator()(const pnmc::mc::places::nopost_live&) const noexcept;
+};
+
+/*------------------------------------------------------------------------------------------------*/
+
+} // namespace std
+
+#endif // _PNMC_MC_PLACES_NOPOST_LIVE_HH_
